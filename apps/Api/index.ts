e02873f14b27b67799f7a1431ae038d3ef5af735
Anurag Singh <anurags9693@gmail.com>
2025-03-18 13:02:02 +0500

    add server listening

--- apps/Api/index.ts
+++ apps/Api/index.ts
@@ -1,11 +1,10 @@
-import express  from "express";
-import {websiteRouter} from './Routes/website'
+import express from "express";
+import { websiteRouter } from './Routes/website'
 import cors from 'cors'
 import { authMiddleware } from "./middlewares/Authmiddlewares";
 
-
-const app  = express.Router()
-
+const app = express();
+const PORT = 8000;
 cors({
     origin: "*"
 })
@@ -13,5 +12,8 @@ cors({
 app.use('*', authMiddleware)
 app.use('/api', websiteRouter)
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
