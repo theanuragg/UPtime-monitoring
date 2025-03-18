68452c221d8d7e8430a29bf9f1e121fc93188bf9
Anurag Singh <anurags9693@gmail.com>
2025-03-18 14:15:42 +0500

    Integrate Clerk authentication middleware

--- apps/Api/middlewares/Authmiddlewares.ts
+++ apps/Api/middlewares/Authmiddlewares.ts
@@ -1,8 +1,12 @@
-import type { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+export default clerkMiddleware();
 
-    
-    next()
-}
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
