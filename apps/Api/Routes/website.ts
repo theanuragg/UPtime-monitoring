3f02e5acdca6552b41e2c34d65c21da4c1d14b85
Anurag Singh <anurags9693@gmail.com>
2025-04-16 10:38:32 +0500

    Change website status endpoint from GET to POST

--- apps/Api/Routes/website.ts
+++ apps/Api/Routes/website.ts
@@ -4,6 +4,6 @@ import  { createWebsite, getWebsiteStatus, getWebsites, deleteWebsite,} from "..
 export const websiteRouter = express.Router()
 
 websiteRouter.post("/v1/website", createWebsite)
-websiteRouter.get("/v1/website/status", getWebsiteStatus)
+websiteRouter.post("/v1/website/status", getWebsiteStatus)
 websiteRouter.get("/v1/websites", getWebsites)
 websiteRouter.delete("/v1/website", deleteWebsite)
